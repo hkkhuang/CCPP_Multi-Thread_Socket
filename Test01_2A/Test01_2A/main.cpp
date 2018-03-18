d030316260dee4b496b01ab8b204a2d538a70c1a
hkkhuang <hkkhuang@163.com>
2018-03-18 19:53:08 +0800

    线程的调度，sleep的使用

---
+++ Test01_2A/Test01_2A/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include "osapi/osapi.h"
+
+//定义一个类
+class Buddhist :public OS_Thread
+{
+	virtual int Routine()
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			printf("和尚念经。。。。\n");
+			//线程 睡眠暂停
+			//OS_Thread::Sleep(1);  //秒为量级
+			OS_Thread::Msleep(100); //毫秒为量级
+		}
+		return 0;
+	}
+};
+int main()
+{
+	Buddhist b;
+	b.Run();
+
+	getchar();
+	return 0;
+}
